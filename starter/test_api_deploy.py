9e1b0be9780057ac6d37ee919927b5b205188ea9
minhnp9 <minhnp9@vingroup.net>
2023-08-27 19:26:40 +0700

    add asset

--- starter/test_api_deploy.py
+++ starter/test_api_deploy.py
@@ -1,24 +1,24 @@
-# import json
-# import requests
+import json
+import requests
 
-# data = {
-#     "age": 30,
-#     "workclass": "State-gov",
-#     "fnlgt": 141297,
-#     "education": "Bachelors",
-#     "education_num": 13,
-#     "marital_status": "Married-civ-spouse",
-#     "occupation": "Prof-specialty",
-#     "relationship": "Husband",
-#     "race": "Asian-Pac-Islander",
-#     "sex": "Male",
-#     "capital_gain": 0,
-#     "capital_loss": 0,
-#     "hours_per_week": 40,
-#     "native_country": "India"
-# }
+data = {
+    "age": 30,
+    "workclass": "State-gov",
+    "fnlgt": 141297,
+    "education": "Bachelors",
+    "education_num": 13,
+    "marital_status": "Married-civ-spouse",
+    "occupation": "Prof-specialty",
+    "relationship": "Husband",
+    "race": "Asian-Pac-Islander",
+    "sex": "Male",
+    "capital_gain": 0,
+    "capital_loss": 0,
+    "hours_per_week": 40,
+    "native_country": "India"
+}
 
-# response = requests.post('https://nd0821-c3-huyenvtk-app.herokuapp.com/', data=json.dumps(data))
+response = requests.post('https://demdecuong-ml-service.onrender.com/', data=json.dumps(data))
 
-# print(response.status_code)
-# print(response.json())
+print(response.status_code)
+print(response.json())
